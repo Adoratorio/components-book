cf7f80de748bd566e4a95b1db60d1cbb0e92b8c6
Daniele | Adoratorio <daniele@adoratorio.com>
2021-05-24 16:18:05 +0200

    Added fully hidden

--- src/components/ComponentsIndex.vue
+++ src/components/ComponentsIndex.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     class="components-book components-index"
-    :class="{ hidden, right: position === 'right' }"
+    :class="{ hidden, fullyHidden, right: position === 'right', dark }"
   >
     <div class="componets-index__inner">
       <div
@@ -102,6 +102,26 @@
     </div>
 
     <div class="components-index__toolbar">
+      <div v-if="dark" class="light" @click="dark = !dark">
+        <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
+          <circle cx="12" cy="12" r="3.25" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"></circle>
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 2.75V4.25"></path>
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17.25 6.75L16.0659 7.93416"></path>
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M21.25 12.0001L19.75 12.0001"></path>
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M17.25 17.2501L16.0659 16.066"></path>
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 19.75V21.25"></path>
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M7.9341 16.0659L6.74996 17.25"></path>
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4.25 12.0001L2.75 12.0001"></path>
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M7.93405 7.93423L6.74991 6.75003"></path>
+        </svg>
+      </div>
+      <div v-else class="dark" @click="dark = !dark">
+        <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M18.25 15.7499C17.2352 16.2904 16.23 16.25 15 16.25C10.9959 16.25 7.75 13.0041 7.75 9.00001C7.75 7.77001 7.70951 6.76474 8.25 5.74994C5.96125 6.96891 4.75 9.2259 4.75 12C4.75 16.004 7.99594 19.25 12 19.25C14.7741 19.25 17.031 18.0387 18.25 15.7499Z"></path>
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M16 4.75C16 6.95914 14.9591 9 12.75 9C14.9591 9 16 11.0409 16 13.25C16 11.0409 17.0409 9 19.25 9C17.0409 9 16 6.95914 16 4.75Z"></path>
+        </svg>
+      </div>
+
       <div v-if="!hidden" class="hide" title="Hide Sidebar" @click="hidden = !hidden">
         <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
           <path
@@ -148,7 +168,12 @@
         </svg>
       </div>
 
-      <div v-if="position === 'right'" class="left" title="Move Sidebar Left" @click="position = 'left'">
+      <div
+        v-if="position === 'right'"
+        class="left"
+        title="Move Sidebar Left"
+        @click="position = 'left'"
+      >
         <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
           <path
             stroke="currentColor"
@@ -166,7 +191,12 @@
           ></path>
         </svg>
       </div>
-      <div v-else class="right" title="Move Sidebar Right" @click="position = 'right'">
+      <div
+        v-else
+        class="right"
+        title="Move Sidebar Right"
+        @click="position = 'right'"
+      >
         <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
           <path
             stroke="currentColor"
@@ -197,13 +227,28 @@ export default {
   data() {
     return {
       hidden: false,
+      fullyHidden: false,
       position: 'left',
+      dark: false,
     };
   },
 
   computed: {
     definitions() {
       return Array.from(definitions);
+    },
+  },
+
+  mounted() {
+    window.addEventListener('keydown', this.keyDown);
+  },
+
+  methods: {
+    keyDown(event) {
+      if (event.keyCode === 72 && event.ctrlKey) {
+        event.preventDefault();
+        this.fullyHidden = !this.fullyHidden;
+      }
     },
   },
 };
@@ -216,7 +261,7 @@ export default {
   left: 40px;
   padding: 20px 10px;
   border-radius: 10px;
-  background-color: #FAFAFA;
+  background-color: #f8f8f8;
   height: calc(100vh - 80px);
   box-sizing: border-box;
   min-width: 240px;
@@ -224,10 +269,19 @@ export default {
   overflow-y: scroll;
 
   font-family: 'San Francisco', Helvetica, Arial, sans-serif;
-  letter-spacing: 0.05em;
+  font-weight: bold;
+  letter-spacing: 0.02em;
   font-size: 12px;
 
   box-shadow: 0px 5px 30px rgba($color: black, $alpha: 0.17);
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+
+  &.fullyHidden {
+    display: none;
+  }
 
   &.hidden {
     opacity: 0.1;
@@ -237,6 +291,19 @@ export default {
   &.right {
     left: auto;
     right: 40px;
+  }
+
+  &.dark {
+    background-color: #171717;
+    color: #FAFAFA;
+
+    & .component__main {
+      background-color: #292929;
+    }
+
+    & .components-index__toolbar > div:hover {
+      background-color: #292929;
+    }
   }
 
   .component {
@@ -329,8 +396,24 @@ export default {
       pointer-events: all;
     }
 
+    @at-root .right & {
+      right: auto;
+      left: 20px;
+    }
+
     > div {
       cursor: pointer;
+      width: 35px;
+      height: 35px;
+      border-radius: 10px;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      &:hover {
+        background-color: #f0f0f0;
+      }
     }
   }
 }
