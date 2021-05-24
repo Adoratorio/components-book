7da871c70144ec2157263cb06c2f03df3278b7a6
Daniele | Adoratorio <daniele@adoratorio.com>
2021-05-24 16:40:54 +0200

    Props binding

--- src/views/Book.vue
+++ src/views/Book.vue
@@ -2,18 +2,34 @@
   <div class="components-book book">
     <ComponentsIndex />
 
-    <component :is="$route.params.component" :props="$route.params.preset" />
+    <component
+      :is="$route.params.component"
+      v-bind="presetProps"
+    />
   </div>
 </template>
 
 <script>
 import ComponentsIndex from '../components/ComponentsIndex.vue';
+import definitions from '../assets/definitions';
 
 export default {
   name: 'Book',
 
   components: {
     ComponentsIndex,
+  },
+
+  computed: {
+    definitions() {
+      return definitions;
+    },
+    presetProps() {
+      const { props } = definitions
+        .get(this.$route.params.component)
+        .presets[this.$route.params.preset];
+      return typeof props === 'function' ? props() : props;
+    },
   },
 };
 </script>
